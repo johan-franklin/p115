3883c1d54b6bc380c803bd90f85c53287e1e5c0c
johan-franklin <87813497+johan-franklin@users.noreply.github.com>
2022-03-24 17:54:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,44 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+  
+       mustache= loadImage('https://i.postimg.cc/nVPrvYkN/OIP-removebg-preview.png');
+        }
+        
+
+
+
+function setup() {
+    canvas = createCanvas(300,300);
+canvas.center();
+Video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+
+poseNet - ml5.poseNet(video, modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+
+function modelLoaded(){
+    console.log("poseNet Is Initialized"); 
+}
+
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.x-15;
+        noseY = results[0].pose.nose.y-15;
+        console.log("nose x = " +results[0].pose.nose.x);
+       console.log("nose y = " +results[0].pose.nose.y);
+    }
+
+
+
+
+    function take_snapshot(){
+        save('myFilterImage.png');
+    }}
